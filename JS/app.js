fa379dfda8ee80e4c914a41a684b7025005daabb
donatusbishung <donaldbishung@yahoo.com>
2023-12-02 07:19:24 +0100

    working on the counter

--- JS/app.js
+++ JS/app.js
@@ -12,10 +12,36 @@ const loopArr = (index) => {
 
     setTimeout(() => {
         loopArr(index);
-    }, 9000);
+    }, 10000);
     
     }
     
 }
 
 loopArr(0)
+
+// Downloadss
+
+// const myDownloads = document.querySelector('.downloads')
+
+// // console.log(myDownloads.innerText)
+// let arrDownloads = [200.344, 200.543, 200.633, 202.342, 202.402, 207.070, 212.332, 217.00, 219.231]
+
+// const downloadsCount = (index) => {
+//     if (index < arrDownloads.length) {
+//         const currentDownload = arrDownloads[index]
+
+//         myDownloads.innerText = `${currentDownload}`;
+
+//         index++;
+
+//         myDownloads.addEventListener('mousemove', setTimeout(() => {
+//             downloadsCount(index);
+//         }, 50))
+        
+//     }
+// }
+
+// // myDownloads.addEventListener('mousemove', downloadsCount(0))
+
+// downloadsCount(0)
