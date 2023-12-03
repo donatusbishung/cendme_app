65bc0cff3a3c4d7d78484c5f0420ab36f1426bd0
Ridwan <aladebaruwa@gmail.com>
2023-12-03 16:45:22 +0100

    add

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+// Contact JS code
+// Function to handle form submission
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get form data
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Construct the mailto URL
+    var mailtoUrl = 'mailto:company@example.com' +
+                    '?subject=' + encodeURIComponent('New Contact Form Submission') +
+                    '&body=' + encodeURIComponent('Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message);
+
+    // Open the user's default email client
+    window.location.href = mailtoUrl;
+}
+
+// Attach the event handler to the form
+document.getElementById('contactForm').addEventListener('submit', handleFormSubmit);
